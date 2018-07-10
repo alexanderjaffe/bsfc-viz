fb0c90cfb69e6046276cb9e5c3c6859dc95f0b06
Alexander Jaffe <alexanderjaffe@Alexanders-MBP.hsd1.ca.comcast.net>
2018-07-10 00:27:29 -0700

    implemented gdoc metadata feature

--- jsonize.py
+++ jsonize.py
@@ -1,8 +1,6 @@
-import re
-import datetime
-import sqlite3
+import re, datetime, sqlite3, gspread
 import pandas as pd
-import random
+from oauth2client.service_account import ServiceAccountCredentials
 
 # trim out excess symbols, specific edits
 def cleanup_name(field):
@@ -27,7 +25,7 @@ def cleanup_name(field):
         return new.replace("Pro Bar", "ProBar")
     else: return new
 
-# trim out excess symbols and chars
+'''# trim out excess symbols and chars
 def trim_category(field):
 
     #m = re.search("(.*)?[\[]*.+", field)
@@ -36,63 +34,44 @@ def trim_category(field):
         return "Other"
     else:
         temp = field.replace(" [Cashier Code: - ]", "")
-        return temp.replace(" [Cashier Code: ' ]", "")
-
-def assign_subcat(name, subcats):
-
-    try:
-        if subcats[name] == "":
-            return "Other"
-        else: return subcats[name]
-    except:
-        return "Other"
-
-def assign_brand(field, brands):
-
-    final = ""
-    for brand in brands:
-        if brand in field:
-            final = brand
-            break
-
-    if final != "":
-        return final
-    else: return "Other"
+        return temp.replace(" [Cashier Code: ' ]", "")'''
 
 # change dates to midnight for aggregation
 def midnight(item):
 
     d = datetime.datetime.strptime(item, "%Y-%m-%d %H:%M:%S.%f")
-    #d = datetime.datetime.fromtimestamp(item/1000.0)
-    #d = datetime.datetime.strftime("%Y-%m-%d 00:00:00")
 
     return d.strftime("%Y-%m-%d")
 
 def main():
 
+    # set up sql query
     QUERY = '''SELECT item_item.id, name, category, price, price_type, unit_name, \
         item_cost, store_use, spoilage, food_prep, committee, sold, member_discount_applied,
             pif_discount_applied, misc_discount_applied, created_at FROM item_item \
             INNER JOIN cost_cost ON item_item.cost_id = cost_cost.id \
             INNER JOIN revenue_revenue ON item_item.revenue_id = revenue_revenue.id'''
-    brands = [line.strip("\n") for line in open("data/brands.txt")]
 
-    # read in manual subcat info
-    subcats = {}
-    for line in open("data/subcats.txt"):
-        subcats[line.split("\t")[0]] = line.split("\t")[1].strip("\n")
+    # set up google API credientials
+    scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('bsfc-inventory-credentials.json', scope)
+    gc = gspread.authorize(credentials)
+
+    # read in meta data from google doc
+    worksheet = gc.open("bsfc inventory test").sheet1
+    all_worksheet_vals = worksheet.get_all_values()
+    # remove header and convert to pandas
+    metadata = pd.DataFrame(all_worksheet_vals[1:])
+    metadata.columns = ["key", "brand", "new_cat", "sub_cat"]
 
     # read sql query into table
-    #items_df1 = pd.read_sql(QUERY, sqlite3.connect('data/db.sqlite3'))
-    #items_df2 = pd.read_sql(QUERY, sqlite3.connect('data/db2.sqlite3'))
-    #items_df = pd.concat([items_df1, items_df2])
     items_df = pd.read_sql(QUERY, sqlite3.connect('data/db_may.sqlite3'))
-
     # trim and modify fields
     items_df["key"] = items_df["name"].apply(cleanup_name)
-    items_df["brand"] = items_df["name"].apply(lambda x: assign_brand(x, brands))
-    items_df["new_cat"] = items_df["category"].apply(lambda x: trim_category(str(x)))
-    items_df["sub_cat"] = items_df["key"].apply(lambda x: assign_subcat(x, subcats))
+    # and merge
+    items_df = pd.merge(items_df, metadata, on="key")
+    #items_df["sub_cat"] = items_df["key"].apply(lambda x: assign_subcat(x, subcats))
     items_df["new_date"] = items_df.created_at.apply(midnight)
 
     # remove old fields
